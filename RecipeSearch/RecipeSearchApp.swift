05fa6a4f5b90692ca70079252ee71cfb793d1a03
Kathryn Verkhohliad <verkhogliadkate@gmail.com>
2024-11-20 03:37:58 +0100

    Initial commit

---
+++ RecipeSearch/RecipeSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeSearchApp.swift
+//  RecipeSearch
+//
+//  Created by Kathryn Verkhogliad on 20.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct RecipeSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
